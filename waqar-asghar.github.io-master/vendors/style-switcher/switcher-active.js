a4af1e0478154eaf824d00eadb7505de77a4397e
Waqar Asghar <waqarasghar497@outlook.com>
2019-03-07 22:02:18 +0500

    update resume

--- waqar-asghar.github.io-master/vendors/style-switcher/switcher-active.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$('#toggle-switcher').click(function(){if($(this).hasClass('open')){$(this).removeClass('open');$('#switch-style').animate({'left':'-200px'});}else{$(this).addClass('open');$('#switch-style').animate({'left':'0'});}});});
